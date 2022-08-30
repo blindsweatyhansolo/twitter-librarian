b7a754d36acfd03f0990fb6a89acfb271aa2c11c
J Semidey <mtsemidey@gmail.com>
2022-08-29 20:53:01 -0400

    fixed config

--- src/config.rs
+++ src/config.rs
@@ -10,7 +10,8 @@ pub struct Config {
     pub TWT_API_KEY_PIRATE: String,
     pub TWT_API_SECRET_PIRATE: String,
     pub TWT_BEARER_TOKEN_PIRATE: String,
-    pub TWT_BEARER_TOKEN_SECRET_PIRATE: String,
+    pub TWT_ACCESS_TOKEN_PIRATE: String,
+    pub TWT_ACCESS_TOKEN_SECRET_PIRATE: String,
 }
 
 impl Config {
@@ -26,13 +27,15 @@ impl Config {
         let TWT_API_KEY_PIRATE = env::var("TWT_API_KEY_PIRATE")?;
         let TWT_API_SECRET_PIRATE = env::var("TWT_API_SECRET_PIRATE")?;
         let TWT_BEARER_TOKEN_PIRATE = env::var("TWT_BEARER_TOKEN_PIRATE")?;
-        let TWT_BEARER_TOKEN_SECRET_PIRATE = env::var("TWT_BEARER_TOKEN_SECRET_PIRATE")?;
+        let TWT_ACCESS_TOKEN_PIRATE = env::var("TWT_ACCESS_TOKEN_PIRATE")?;
+        let TWT_ACCESS_TOKEN_SECRET_PIRATE = env::var("TWT_ACCESS_TOKEN_SECRET_PIRATE")?;
 
         Ok(Config {
             TWT_API_KEY_PIRATE,
             TWT_API_SECRET_PIRATE,
             TWT_BEARER_TOKEN_PIRATE,
-            TWT_BEARER_TOKEN_SECRET_PIRATE
+            TWT_ACCESS_TOKEN_PIRATE,
+            TWT_ACCESS_TOKEN_SECRET_PIRATE,
         })
     }
 }
